18df5dc87c3aaf64e1f617335686f98f80deb48a
croope@gmail.com <croope@gmail.com>
2017-08-16 18:01:24 +0100

    Updated example code

--- index.android.js
+++ index.android.js
@@ -4,50 +4,67 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+var MOCKED_MOVIES_DATA = [
+	{title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
 export default class reactAnd extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+	onClick = (event, data) => {
+		
+	}
+
+	render() {
+		var movie = MOCKED_MOVIES_DATA[0];
+
+		return (
+		<View style={styles.container}>
+			<Text
+				onClick={this.onClick}
+				style={styles.welcome}>
+				Welcome to React Native!
+			</Text>
+			<Text style={styles.instructions}>
+				{movie.title}
+			</Text>
+			<Text style={styles.instructions}>
+				{movie.year}
+			</Text>
+			<Image style={styles.thumbnail} source={{uri: movie.posters.thumbnail}} />
+		</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10,
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5,
+	},
+	thumbnail: {
+		width: 53,
+		height: 81,
+	}
 });
 
 AppRegistry.registerComponent('reactAnd', () => reactAnd);
